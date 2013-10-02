22c7ba59724d3aa657950381250f10e46d9e43c4
fazreil@gmail.com <fazreil@gmail.com@75c35466-a88c-8032-13a9-edaddaf0d33c>
2013-10-02 06:29:21 +0000

    issue-9 - branch out for surefire plugin

---
+++ branches/issue_9/src/main/java/org/gravity/Math.java
@@ -0,0 +1,83 @@
+package org.gravity;
+
+import java.io.IOException;
+
+public class Math{
+	/** 
+	 * Math function: Plus
+	 * 
+	 * <p>
+	 * Execute plus operation
+	 * <p>
+	 * @param input x value
+	 * @param constant y value
+	 * <p>
+	 * Example:
+	 * <p>
+	 * int out = math.plus(10,2);
+	 */
+	public static int plus (int input, int constant) throws IOException{
+		int x = input;
+		int y = constant;
+		int z = x+y;
+		return z;
+	}
+	/** 
+	 * Math function: Minus
+	 * 
+	 * <p>
+	 * Execute Minus operation
+	 * <p>
+	 * @param input x value
+	 * @param constant y value
+	 * <p>
+	 * Example:
+	 * <p>
+	 * int out = math.minus(10,2);
+	 */
+	public static int minus (int input, int constant) throws IOException{
+		int x = input;
+		int y = constant;
+		int z = x-y;
+		return z;
+	}
+	/** 
+	 * Math function: Multiple
+	 * 
+	 * <p>
+	 * Execute Multiple operation
+	 * <p>
+	 * @param input x value
+	 * @param constant y value
+	 * <p>
+	 * Example:
+	 * <p>
+	 * int out = math.multiple(10,2);
+	 */
+	public static int multiple (int input, int constant) throws IOException{
+		int x = input;
+		int y = constant;
+		int z = x*y;
+		return z;
+	}
+	/** 
+	 * Math function: Divide
+	 * 
+	 * <p>
+	 * Execute Divide operation
+	 * <p>
+	 * @param input x value
+	 * @param constant y value
+	 * <p>
+	 * Example:
+	 * <p>
+	 * int out = math.divide(10,2);
+	 */
+	public static int divide (int input, int constant) throws IOException{
+		int x = input;
+		int y = constant;
+		int z = x/y;
+		return z;
+	}
+	
+}
