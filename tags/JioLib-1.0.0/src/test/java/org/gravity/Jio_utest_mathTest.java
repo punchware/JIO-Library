1336ad736aabc9c8d03dcd3c5f45a4050210052f
fazreil@gmail.com <fazreil@gmail.com@75c35466-a88c-8032-13a9-edaddaf0d33c>
2013-10-25 12:32:47 +0000

    [maven-release-plugin]  copy for tag JioLib-1.0.0

---
+++ tags/JioLib-1.0.0/src/test/java/org/gravity/Jio_utest_mathTest.java
@@ -0,0 +1,114 @@
+package org.gravity;
+import java.io.IOException;
+
+import junit.framework.Assert;
+import junit.framework.AssertionFailedError;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Jio_utest_mathTest {
+	@Before
+	public void testSetup()
+	{
+		System.out.println("math class unit Test start.");
+	}
+
+	@After
+	public void testComplete()
+	{
+		System.out.println("\nMath class unit test complete.");
+	}
+
+	
+	@Test
+	 public void TestMath_01_plus() throws IOException{
+		int x =Math.plus(1, 1);
+		//Positive test
+		try{
+			Assert.assertEquals(2, x);
+			System.out.print("TestMath_01_plus_Positive test PASSED, actual value: "+x+", ");
+		}
+			catch (AssertionFailedError e){
+			System.out.println("TestMath_01_plus_Positive test FAIL: "+e.getMessage());
+			}
+		//Negative test
+		try{	
+			Assert.assertNotSame(1, x);
+			System.out.print("TestMath_01_plus_Negative test PASSED, actual value: "+x);
+		}
+			catch (AssertionFailedError e){
+			System.out.println("TestMath_01_plus_Negative test FAIL: "+e.getMessage());
+			}
+			
+		}
+	
+	@Test
+	 public void TestMath_02_minus() throws IOException{
+		int x =Math.minus(2, 1);
+		//Positive test
+		try{
+			Assert.assertEquals(1, x);
+			System.out.print("TestMath_02_minus_Positive test PASSED, actual value: "+x+", ");
+		}
+			catch (AssertionFailedError e){
+			System.out.println("TestMath_02_minus_Positive test FAIL: "+e.getMessage());
+			}
+		//Negative test
+		try{	
+			Assert.assertNotSame(2, x);
+			System.out.print("TestMath_02_minus_Negative test PASSED, actual value: "+x);
+		}
+			catch (AssertionFailedError e){
+			System.out.println("TestMath_02_minus_Negative test FAIL: "+e.getMessage());
+			}
+			
+		}
+	
+	@Test
+	 public void TestMath_03_multiple() throws IOException{
+		int x =Math.multiple(2, 2);
+		//Positive test
+		try{
+			Assert.assertEquals(4, x);
+			System.out.print("TestMath_03_multiple_Positive test PASSED, actual value: "+x+", ");
+		}
+			catch (AssertionFailedError e){
+			System.out.println("TestMath_03_multiple_plus_Positive test FAIL: "+e.getMessage());
+			}
+		//Negative test
+		try{	
+			Assert.assertNotSame(2, x);
+			System.out.print("TestMath_03_multiple_Negative test PASSED, actual value: "+x);
+		}
+			catch (AssertionFailedError e){
+			System.out.println("TestMath_03_multiple_Negative test FAIL: "+e.getMessage());
+			}
+			
+		}
+
+	@Test
+	 public void TestMath_04_divide() throws IOException{
+		int x =Math.divide(4, 2);
+		//Positive test
+		try{
+			Assert.assertEquals(2, x);
+			System.out.print("TestMath_04_divide_Positive test PASSED, actual value: "+x+", ");
+		}
+			catch (AssertionFailedError e){
+			System.out.println("TestMath_04_divide_plus_Positive test FAIL: "+e.getMessage());
+			}
+		//Negative test
+		try{	
+			Assert.assertNotSame(1, x);
+			System.out.print("TestMath_04_divide_Negative test PASSED, actual value: "+x);
+		}
+			catch (AssertionFailedError e){
+			System.out.println("TestMath_04_divide_Negative test FAIL: "+e.getMessage());
+			}
+			
+		}
+	
+
+}
