7379a7f02065077f69652604df869af25aa7b25a
fazreil <fazreil@gmail.com>
2015-09-21 20:19:25 +0800

    #25 #26 #27 conversion java source code

--- src/main/java/org/gravity/Conversion.java
+++ src/main/java/org/gravity/Conversion.java
@@ -7,6 +7,7 @@ public class Conversion {
 	public final double KILOMETER = 1000;
 	public final double INCH = 39.3701;
 	public final double FEET = 3.28084;
+	public final double MILE = 0.000621371;
 	
 	public static double convert(double value, double from, double to){
 		if(from != 1){
@@ -18,7 +19,7 @@ public class Conversion {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Conversion.convert(1,new Conversion().CENTIMETER,new Conversion().FEET));
+		System.out.println(Conversion.convert(1,new Conversion().KILOMETER,new Conversion().MILE));
 	}
 
 }
