c80b84e91f5c51913c6b8a801b4de9f54793f05c
Fazreil Amreen <fazreil@gmail.com>
2017-04-03 09:07:17 +0800

    Revert "new stuff for 1.3.0"

--- src/main/java/org/gravity/window/Window.java
+++ src/main/java/org/gravity/window/Window.java
@@ -2,45 +2,25 @@ package org.gravity.window;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class Window extends java.awt.Frame{
 	
 	public Window()
 	{
 		initializeWithCommonBehavior();
-		configureFrame();
-		fillFrame();
 		draw();
 
 	}
 
 	public void initializeWithCommonBehavior(){
 		this.addWindowListener(new JIOWindowAdapter());
-		this.setSize(320,240);
 	}
-
-	public void configureFrame(){}
-
-	public void fillFrame(){}
 
 	public void draw(){
-		this.setVisible(true);
+
 	}
 
-	protected void testNull(File file){
-		if(file == null){
-			System.out.println("null file");
-		}
-		else{
-			System.out.println("not null file");	
-		}
-	}
-	
-	public static void main(String[] args)
+	public static void Main(String[] args)
 	{
 		Window win = new Window();
 	}
