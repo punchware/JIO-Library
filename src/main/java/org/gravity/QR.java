e59a6b584aaf3e1a18fe67621aba52ac2cb45e20
fazreil <fazreil@gmail.com>
2017-04-05 02:18:18 +0800

    RGB QR code decoder!!!

--- src/main/java/org/gravity/QR.java
+++ src/main/java/org/gravity/QR.java
@@ -36,23 +36,26 @@ public class QR {
 	private String message = null;
 	private String path = null;
 	private String filename = null;
-	
-	public QR(){
-		
+
+	public QR() {
+
 	}
-	
+
 	/**
 	 * Constructor with 3 simple parameter to produce a QR
 	 * 
-	 * @param message the message to encode into QR code
-	 * @param path the path where the QR code image to be placed
-	 * @param filename the name of the QR code file to be generated
-	 * <p>
-	 * Example:
-	 * <p>
-	 * QR qr = new QR("foo bar","/path/to/image","foobar");
+	 * @param message
+	 *            the message to encode into QR code
+	 * @param path
+	 *            the path where the QR code image to be placed
+	 * @param filename
+	 *            the name of the QR code file to be generated
+	 *            <p>
+	 *            Example:
+	 *            <p>
+	 *            QR qr = new QR("foo bar","/path/to/image","foobar");
 	 */
-	public QR(String message, String path, String filename){
+	public QR(String message, String path, String filename) {
 		this.message = message;
 		this.path = path;
 		this.filename = filename;
@@ -61,263 +64,365 @@ public class QR {
 	/**
 	 * pojo function to get message
 	 *
-	 * @return	 	message behind QRcode
+	 * @return message behind QRcode
 	 */
-	public String getMessage(){
+	public String getMessage() {
 		return message;
 	}
 
 	/**
 	 * pojo function to set message
 	 *
-	 * @param message String to set as message 
+	 * @param message
+	 *            String to set as message
 	 */
-	public void setMessage(String message){
+	public void setMessage(String message) {
 		this.message = message;
 	}
 
 	/**
 	 * pojo function to get path in String
 	 *
-	 * @return		path of QR file
+	 * @return path of QR file
 	 */
-	public String getPath(){
+	public String getPath() {
 		return path;
 	}
 
 	/**
 	 * pojo function to set path
 	 *
-	 * @param path String to set as path
+	 * @param path
+	 *            String to set as path
 	 */
-	public void setPath(String path){
+	public void setPath(String path) {
 		this.path = path;
 	}
 
 	/**
 	 * pojo function to get filename in String
 	 *
-	 * @return		filename of QR
+	 * @return filename of QR
 	 */
-	public String getFilename(){
+	public String getFilename() {
 		return filename;
 	}
 
 	/**
 	 * pojo function to set filename
 	 *
-	 * @param filename String to set as filename
+	 * @param filename
+	 *            String to set as filename
 	 */
-	public void setFilename(String filename){
+	public void setFilename(String filename) {
 		this.filename = filename;
 	}
 
 	/**
 	 * Encodes message in this.message to qr code bufferedImage
 	 *
-	 * @return		bufferedImage of QR, which is meant to be written to a file of to be manipulated into other image usages
+	 * @return bufferedImage of QR, which is meant to be written to a file of to
+	 *         be manipulated into other image usages
 	 */
-	public BufferedImage encodeQR()
-	{
+	public BufferedImage encodeQR() {
 		QRCodeWriter qrWriter = new QRCodeWriter();
 		BitMatrix bitmatrix = new BitMatrix(128, 128);
 		BufferedImage bufferedImage = new BufferedImage(128, 128, BufferedImage.TYPE_INT_RGB);
-		
-		try{
+
+		try {
 			bitmatrix = qrWriter.encode(message, BarcodeFormat.QR_CODE, 128, 128);
 			bufferedImage.createGraphics();
-			
+
 			Graphics2D graphics2d = (Graphics2D) bufferedImage.getGraphics();
 			graphics2d.setColor(Color.WHITE);
 			graphics2d.fillRect(0, 0, 128, 128);
 			graphics2d.setColor(Color.BLACK);
 			for (int i = 0; i < 128; i++) {
-	            for (int j = 0; j < 128; j++) {
-	                if (bitmatrix.get(i, j)) {
-	                    graphics2d.fillRect(i, j, 1, 1);
-	                }
-	            }
-	        }
-			
-		}
-		catch(Exception ex){
+				for (int j = 0; j < 128; j++) {
+					if (bitmatrix.get(i, j)) {
+						graphics2d.fillRect(i, j, 1, 1);
+					}
+				}
+			}
+
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		return bufferedImage;
-
 	}
-	
-	public BufferedImage encodeRGBQR(){
+
+	/**
+	 * Encodes message in this.message to qr code bufferedImage
+	 *
+	 * @return bufferedImage of QR, which is meant to be written to a file of to
+	 *         be manipulated into other image usages
+	 */
+	public BufferedImage encodeQR(Color color) {
 		QRCodeWriter qrWriter = new QRCodeWriter();
-		BitMatrix bitMatrixR = new BitMatrix(128,128);
-		BitMatrix bitMatrixG = new BitMatrix(128,128);
-		BitMatrix bitMatrixB = new BitMatrix(128,128);
-		BufferedImage bufferedImage = new BufferedImage(128,128,BufferedImage.TYPE_INT_RGB);
-		
+		BitMatrix bitmatrix = new BitMatrix(128, 128);
+		BufferedImage bufferedImage = new BufferedImage(128, 128, BufferedImage.TYPE_INT_RGB);
+
+		try {
+			bitmatrix = qrWriter.encode(message, BarcodeFormat.QR_CODE, 128, 128);
+			bufferedImage.createGraphics();
+
+			Graphics2D graphics2d = (Graphics2D) bufferedImage.getGraphics();
+			graphics2d.setColor(Color.WHITE);
+			graphics2d.fillRect(0, 0, 128, 128);
+			graphics2d.setColor(color);
+			for (int i = 0; i < 128; i++) {
+				for (int j = 0; j < 128; j++) {
+					if (bitmatrix.get(i, j)) {
+						graphics2d.fillRect(i, j, 1, 1);
+					}
+				}
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return bufferedImage;
+	}
+
+	public BufferedImage encodeRGBQR() {
+		QRCodeWriter qrWriter = new QRCodeWriter();
+		BitMatrix bitMatrixR = new BitMatrix(128, 128);
+		BitMatrix bitMatrixG = new BitMatrix(128, 128);
+		BitMatrix bitMatrixB = new BitMatrix(128, 128);
+		BufferedImage bufferedImage = new BufferedImage(128, 128, BufferedImage.TYPE_INT_RGB);
+
 		String[] messages = breakMessagesIntoThree(message);
-		
-		try{
+
+		try {
 			bitMatrixR = qrWriter.encode(messages[0], BarcodeFormat.QR_CODE, 128, 128);
 			bitMatrixG = qrWriter.encode(messages[1], BarcodeFormat.QR_CODE, 128, 128);
 			bitMatrixB = qrWriter.encode(messages[2], BarcodeFormat.QR_CODE, 128, 128);
 			bufferedImage.createGraphics();
-			
+
 			Graphics2D graphics2d = (Graphics2D) bufferedImage.getGraphics();
 			graphics2d.setColor(Color.WHITE);
 			graphics2d.fillRect(0, 0, 128, 128);
-			for(int i=0;i<128;i++){
-				for(int j=0;j<128;j++){
-					if(bitMatrixR.get(i,j)){
-						if(bitMatrixG.get(i,j)){
-							if(bitMatrixB.get(i, j)){
-								graphics2d.setColor(Color.BLACK);
-							}
-							else{
-								graphics2d.setColor(Color.YELLOW);
-							}
-						}
-						else if(bitMatrixB.get(i, j)){
-							graphics2d.setColor(Color.MAGENTA);
-						}
-						else{
-							graphics2d.setColor(Color.RED);
-						}
-					}
-					else if(bitMatrixG.get(i, j)){
-						if(bitMatrixB.get(i, j)){
-							graphics2d.setColor(Color.CYAN);
-						}
-						else{
-							graphics2d.setColor(Color.GREEN);
-						}
-					}
-					else if(bitMatrixB.get(i, j)){
-						graphics2d.setColor(Color.BLUE);
-					}
-					else{
-						graphics2d.setColor(Color.WHITE);
-					}
+			for (int i = 0; i < 128; i++) {
+				for (int j = 0; j < 128; j++) {
+					graphics2d.setColor(colorToFill(bitMatrixR, bitMatrixG, bitMatrixB, i, j));
 					graphics2d.fillRect(i, j, 1, 1);
 				}
 			}
-		}
-		catch(Exception ex){
+
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		
 		return bufferedImage;
-		
 	}
-	
-	private String[] breakMessagesIntoThree(String message){
-		String[] messages = {null,null,null};
+
+	private Color colorToFill(BitMatrix bitMatrixR, BitMatrix bitMatrixG, BitMatrix bitMatrixB, int i, int j) {
+		if (bitMatrixR.get(i, j)) {
+			if (bitMatrixG.get(i, j)) {
+				if (bitMatrixB.get(i, j)) {
+					return Color.BLACK;
+				} else {
+					return Color.YELLOW;
+				}
+			} else if (bitMatrixB.get(i, j)) {
+				return Color.MAGENTA;
+			} else {
+				return Color.RED;
+			}
+		} else if (bitMatrixG.get(i, j)) {
+			if (bitMatrixB.get(i, j)) {
+				return Color.CYAN;
+			} else {
+				return Color.GREEN;
+			}
+		} else if (bitMatrixB.get(i, j)) {
+			return Color.BLUE;
+		} else {
+			return Color.WHITE;
+		}
+	}
+
+	public String[] breakMessagesIntoThree(String message) {
+		String[] messages = { null, null, null };
+
+		int length = 0;
 		
-		int length = (message.length())/3;
-		
-		messages[0] = message.substring(0,length);
-		messages[1] = message.substring(length+1, length+length);
-		messages[2] = message.substring(length+length+1);
-		
+		if(length%3==0)
+		{
+			length = (message.length()) / 3;
+		}
+		else{
+			for(int i=0;i<(message.length()%3);i++)
+			message = message+" ";
+		}
+		messages[0] = message.substring(0, length);
+		messages[1] = message.substring(length, length + length);
+		messages[2] = message.substring(length + length);
+
 		return messages;
 	}
 
 	/**
 	 * creates this.filename as png image in the path given in this.path
 	 *
-	 * @param bufferedImage is the image passed down from encodeQR function
+	 * @param bufferedImage
+	 *            is the image passed down from encodeQR function
 	 */
-	public void generateQR(BufferedImage bufferedImage)
-	{
-		
-		try{
-			
-			String filePath = path+filename+".png";
-			System.out.println("output path: "+new File(filePath).getAbsolutePath());
+	public void generateQR(BufferedImage bufferedImage) {
+
+		try {
+
+			String filePath = path + filename + ".png";
+			System.out.println("output path: " + new File(filePath).getAbsolutePath());
 			ImageIO.write(bufferedImage, "png", new File(filePath));
-			
-		}
-		catch(Exception ex){
+
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
 
-	public byte[] generateQR(BufferedImage bufferedImage,ImageOutputStream ioStream, File file)
-	{
+	public File generateQR(BufferedImage bufferedImage, File file) {
+
+		try {
+
+			ImageIO.write(bufferedImage, "png", file);
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return file;
+	}
+
+	public byte[] generateQR(BufferedImage bufferedImage, ImageOutputStream ioStream, File file) {
 
 		ByteArrayOutputStream bos = new ByteArrayOutputStream(255);
 		byte[] byteArray = new byte[255];
-		try{
+		try {
 			ioStream = ImageIO.createImageOutputStream(bos);
 			ImageIO.write(bufferedImage, "png", ioStream);
 			ImageIO.write(bufferedImage, "png", file);
 			ioStream.write(byteArray);
-		}
-		catch(Exception ex){
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		return byteArray;
 	}
-	
-	public String readQR(File file){
+
+	public String readQR(File file) {
 		String qrDecoded = null;
 
-		try{
+		try {
 			BinaryBitmap binaryBitmap = new BinaryBitmap(
-				new HybridBinarizer(
-			        new BufferedImageLuminanceSource(
-			            ImageIO.read(file)
-			            )
-			        ));
+					new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(file))));
 			Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap);
 			qrDecoded = qrCodeResult.getText();
-		}catch(NotFoundException nfEx){
+		} catch (NotFoundException nfEx) {
+			System.out.println(file.getAbsolutePath());
 			nfEx.printStackTrace();
-		}catch(IOException ioEx){
+		} catch (IOException ioEx) {
 			ioEx.printStackTrace();
 		}
 		return qrDecoded;
 	}
-	
-//	public String readRGBQR(File file){
-//		String rgbQrDecoded = null;
-//		try{
-////			BufferedImageLuminanceSource bils = new BufferedImageLuminanceSource(ImageIO.read(file));
-////			
-////			HybridBinarizer hybridBin = new HybridBinarizer(bils);
-////			BinaryBitmap binaryBitmap = new BinaryBitmap(hybridBin);
-////			Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap);
-//			
-//			System.out.println(file.getAbsolutePath());
-//			
-//			File bitmapFile = new File("bitmap.bmp");
-//			ImageIO.write(ImageIO.read(file), "bmp", bitmapFile);
-//			
-//			Bitmap bm = ImageIO.
-//			
-//			for(int i=0;i<bm.getWidth();i++){
-//				for(int j=0;j<bm.getHeight();j++){
-//					System.out.println(bm.getPixel(i, j));
-//				}
-//				System.out.println("\n");
-//			}
-//			
-//			BitMatrix bmR = new BitMatrix(128, 128);
-//			BitMatrix bmG = new BitMatrix(128, 128);
-//			BitMatrix bmB = new BitMatrix(128, 128);
-//			
-//		}
-//		catch(Exception exception){
-//			exception.printStackTrace();
-//		}
-//		
-//		return rgbQrDecoded;
-//	}
 
-	public static void main(String[] args){
-		QR qrCode = new QR(args[0],args[1],args[2]);	
-		qrCode.generateQR(qrCode.encodeQR());
+	public String readRGBQR(File file) {
+		String rgbQrDecoded = null;
+
+		try {
+
+			rgbQrDecoded = fadeToBlackAndReturnMessage(file, Color.RED) + fadeToBlackAndReturnMessage(file, Color.GREEN)
+					+ fadeToBlackAndReturnMessage(file, Color.BLUE);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+
+		return rgbQrDecoded;
 	}
 
+	private String fadeToBlackAndReturnMessage(File file, Color color) {
+		String message = null;
+		BufferedImage bufferedImage = null;
+		int height = 0;
+		int width = 0;
+		int rgb = 0;
+		int red = 0;
+		int green = 0;
+		int blue = 0;
 
-	
+		try {
+			bufferedImage = ImageIO.read(file);
+			height = bufferedImage.getHeight();
+			width = bufferedImage.getWidth();
+
+			for (int h = 0; h < height; h++) {
+				for (int w = 0; w < width; w++) {
+					rgb = bufferedImage.getRGB(h, w);
+					red = (rgb >> 16) & 255;
+					green = (rgb >> 8) & 255;
+					blue = (rgb) & 255;
+
+					if (red == 0 && green == 0 && blue == 0) {
+						bufferedImage.setRGB(h, w, Color.BLACK.getRGB());
+					} 
+					else if (red == 255 && green == 255 && blue == 255) {
+						bufferedImage.setRGB(h, w, Color.WHITE.getRGB());
+					} else {
+						if (color == Color.RED) {
+							if (red == 255) {
+								bufferedImage.setRGB(h, w, Color.BLACK.getRGB());
+							} 
+							else {
+								bufferedImage.setRGB(h, w, Color.WHITE.getRGB());
+							}
+						}
+						if (color == Color.GREEN) {
+							if (green == 255) {
+								bufferedImage.setRGB(h, w, Color.BLACK.getRGB());
+							} 
+							else {
+								bufferedImage.setRGB(h, w, Color.WHITE.getRGB());
+							}
+						}
+						if (color == Color.BLUE) {
+							if (blue == 255) {
+								bufferedImage.setRGB(h, w, Color.BLACK.getRGB());
+							} 
+							else {
+								bufferedImage.setRGB(h, w, Color.WHITE.getRGB());
+							}
+						}
+					}
+				}
+			}
+
+			System.out.println("pre-generateQr");
+			message = readQR(generateQR(bufferedImage, new File("q")));
+			System.out.println(message);
+			System.out.println("post-generateQr");
+
+		} catch (
+
+		IOException ioex) {
+			ioex.printStackTrace();
+		}
+
+		return message;
+	}
+
+	public static void main(String[] args) {
+
+//		QR qrCode = new QR(args[0], args[1], args[2]);
+//		qrCode.generateQR(qrCode.encodeQR());
+//		QR rgbQRCode = new QR(args[0], args[1], args[2]);
+//		rgbQRCode.generateQR(rgbQRCode.encodeRGBQR());
+		
+		QR msg = new QR();
+		msg.setMessage("123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890");
+		String msgs[] = msg.breakMessagesIntoThree(msg.getMessage());
+		
+		for(int a=0;a<msgs.length;a++){
+			System.out.println(msgs[a]);
+		}
+	}
+
 }
